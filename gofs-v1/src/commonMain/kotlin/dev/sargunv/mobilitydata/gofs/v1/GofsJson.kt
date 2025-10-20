c81ebfcc93a1158f520d50c7cd9edb01929b3051
Sargun Vohra <sargunv@users.noreply.github.com>
2025-10-19 20:51:02 -0700

    implement gbfs v1

--- gofs-v1/src/commonMain/kotlin/dev/sargunv/mobilitydata/gofs/v1/GofsJson.kt
+++ gofs-v1/src/commonMain/kotlin/dev/sargunv/mobilitydata/gofs/v1/GofsJson.kt
@@ -2,7 +2,7 @@ package dev.sargunv.mobilitydata.gofs.v1
 
 import kotlinx.serialization.json.Json
 
-/** Pre-configured JSON instance for parsing GOFS v1 feeds. */
+/** Pre-configured JSON instance for parsing GOFS feeds. */
 public val GofsJson: Json = Json {
   explicitNulls = true
   encodeDefaults = false
